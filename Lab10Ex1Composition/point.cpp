4d21f179e5b2d9d4ed695aebd8aa17737e2090a4
loucifMM <m_loucif@hotmail.com>
2018-07-31 21:18:11 -0400

    Modif point.cpp

--- Lab10Ex1Composition/point.cpp
+++ Lab10Ex1Composition/point.cpp
@@ -1,7 +1,7 @@
 #include "point.h"
 
 Point::Point(float _x, float _y):x(_x),y(_y) {
-            //std::cout<<"Construction point"<<std::endl;
+            std::cout<<"Construction point"<<std::endl;
         }
 
 Point::~Point(){
